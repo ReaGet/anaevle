286059644d4ab57fab05d72bdc5945a954a78173
reaget <reaget.acc@gmail.com>
2023-11-07 16:55:07 +0300

    feat: add spiritual markup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import "./css/index.css";
 import { calculate } from "./js/utils";
-import { basic, personality } from "./js/templates";
+import { basic, personality, spirituality } from "./js/templates";
 
-const _date = "03.04.1998";
+const _date = "28.03.1997";
 
 const values = calculate(_date);
 
@@ -10,5 +10,6 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div class="flex flex-col items-center gap-40 min-h-screen bg-gray-950">
     ${basic(values)}
     ${personality([values[0], values[6], values[10]])}
+    ${spirituality([values[0], values[6], values[10]])}
   </div>
 `;
