fb38101d631aa555ff6c14cec3923a63bfaae4a3
reaget <reaget.acc@gmail.com>
2023-11-08 12:32:21 +0300

    chore: last updates

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,5 @@ export default {
   base: "",
   build: {
     outDir: "docs",
-    // rollupOptions: {
-    //   output: {
-    //     chunkFileNames: './assets/[name]-[hash].js',
-    //     entryFileNames: './assets/[name]-[hash].js',
-    //   }
-    // }
   }
 }
